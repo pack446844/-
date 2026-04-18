f7aecfb1557edc208ff5c1d4ca5eeb10b566f95d
pack446813 <pack446844@gmail.com>
2026-04-18 20:17:55 +0900

    Delete Players/F0_8V directory

--- Players/F0_8V/PlayerScripts/Client/AdminAuditLogUIController.LocalScript.lua
+++
@@ -1,7 +0,0 @@
-local v1 = game:GetService("Players").LocalPlayer
-local v2 = v1 and v1:WaitForChild("PlayerGui") or nil
-local v3 = v2 and v2:FindFirstChild("AdminAuditLogGui")
-if v3 then
-    v3:Destroy()
-end
-print("[AdminAuditLogUIController] disabled for data safety")
